629f136e600e80db5859ac4b45ae7756c7164d52
Stephany Ramirez <stephany.ramireze@gmail.com>
2023-10-19 21:17:08 -0700

    add it invoice and invoice items to the schema

---
+++ app/models/invoice.rb
@@ -0,0 +1,18 @@
+class Invoice < ApplicationRecord
+  belongs_to :customer
+  has_many :invoice_items, dependent: :destroy # Depending on your requirements
+
+  accepts_nested_attributes_for :invoice_items, reject_if: :all_blank, allow_destroy: true
+
+  def sub_total
+    invoice_items.sum('quantity * unit_price')
+  end
+
+  def tax
+    0.16 * sub_total
+  end
+
+  def total
+    sub_total + tax
+  end
+end
